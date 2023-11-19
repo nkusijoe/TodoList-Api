4cec07c5a46c06495ca3d89daf41d3c03067a525
nkusijoe <joenkusi01@gmail.com>
2023-11-20 01:38:42 +0200

    updated todoRoutes with more routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const todoController = require('../controllers/todoController');
 
 // Define routes
-router.get('/todos', todoController.getAllTodos);
-router.post('/todos', todoController.createTodo);
+router.get('/', todoController.getAllTasks);
+router.post('/createTask', todoController.createTask);
+router.get('/:id', todoController.getTaskById); // Get a specific todo by ID
+router.put('/:id', todoController.updateTask);  // Update a todo by ID
+router.delete('/:id', todoController.deleteTask); // Delete a todo by ID
 
 module.exports = router;

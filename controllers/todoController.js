492a7328e9df9cdfceb2d94a011cbe1a34e1ec59
nkusijoe <joenkusi01@gmail.com>
2023-11-20 01:30:16 +0200

    updated todoController with all crud properties

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,67 @@
 const Todo = require('../models/todoModel');
 
-// Controller methods
-const getAllTodos = async (req, res) => {
+const getAllTasks = async (req, res) => {
   try {
-    const todos = await Todo.find();
-    res.json(todos);
+    const tasks = await Todo.find();
+    res.json(tasks);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const createTodo = async (req, res) => {
+const createTask = async (req, res) => {
   try {
-    const newTodo = new Todo(req.body);
-    const savedTodo = await newTodo.save();
-    res.status(201).json(savedTodo);
+    const newTask = new Todo(req.body);
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-module.exports = { getAllTodos, createTodo };
+const getTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Todo.findById(id);
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedTask = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!updatedTask) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Todo.findByIdAndDelete(id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    res.json(deletedTask);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllTasks, createTask, getTaskById, updateTask, deleteTask};
